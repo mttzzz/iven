788ed008334dbe177517e4547005ba5c74cbe720
Kirill Egorov <kirill_egorov@me.com>
2024-09-02 15:39:45 +0300

    update server/api/products/index.get.ts

--- server/api/products/index.get.ts
+++ server/api/products/index.get.ts
@@ -1,4 +1,4 @@
 export default defineEventHandler(async (event) => {
   const prisma  = event.context.prisma
-  return await prisma.iven_products.findMany({take: 10})
+  return await prisma.iven_products.findMany({take: 10, skip: Math.floor(Math.random() * 10000)})
 })
